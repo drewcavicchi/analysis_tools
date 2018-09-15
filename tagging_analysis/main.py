5d85edae60918ed196b8ba3520789f4a347c5852
drewcavicchi <38928835+drewcavicchi@users.noreply.github.com>
2018-09-14 18:03:37 -0700

    working prototype
    It works! still needs to be optimized.

--- tagging_analysis/main.py
+++ tagging_analysis/main.py
@@ -1,18 +1,76 @@
 import pandas
+from datetime import datetime
+import csv
+# TODO: automate useful_columns and headers
 
-number_list = ['10-Second Video Views', '3-Second Video Views',
-       'Amount Spent (USD)', 'Clicks (All)',
-       'Frequency', 'Impressions',
-        'Number of Records',
-       'Permalink', 'Post Engagement', 'Reach','Video Average Watch Time',
-       'Video Percentage Watched', 'Video Watches at 100%',
-       'Video Watches at 25%', 'Video Watches at 50%', 'Video Watches at 75%',
-       'Video Watches at 95%','Website Purchases', 'Website Searches']
+startTime = datetime.now()
 
-new_dataframe_columns = ['Tag', 'total_amount_spent', 'total_impressions', 'total_website_purchases', 'total_website_searches',
-        'price_per_purchase', 'price_per_search', 'purchase_rate', 'search_rate', 'purchase_per_search']
-        # TODO: automate new columns
+useful_columns = ['10-Second Video Views',
+                   '3-Second Video Views',
+                   'Ad ID',
+                   'Ad Name',
+                   'Ad Set ID',
+                   'Ad Set Name',
+                   'Ad name 2',
+                   'Amount Spent (USD)',
+                   'Campaign ID',
+                   'Campaign Name',
+                   'Clicks (All)',
+                   'Creative',
+                   'Frequency',
+                   'Impressions',
+                   'Permalink',
+                   'Post Engagement',
+                   'Reach',
+                   'Reporting Ends',
+                   'Reporting Starts',
+                   'Video Average Watch Time',
+                   'Video Percentage Watched',
+                   'Video Watches at 100%',
+                   'Video Watches at 25%',
+                   'Video Watches at 50%',
+                   'Video Watches at 75%',
+                   'Video Watches at 95%',
+                   'Website Purchases',
+                   'Website Searches',
+                   'Ad Name 8'
+                    ]
+headers = [
+                   '10-Second Video Views',
+                   '3-Second Video Views',
+                   'Ad ID',
+                   'Ad Name',
+                   'Ad Set ID',
+                   'Ad Set Name',
+                   'Ad name 2',
+                   'Amount Spent (USD)',
+                   'Campaign ID',
+                   'Campaign Name',
+                   'Clicks (All)',
+                   'Creative',
+                   'Frequency',
+                   'Impressions',
+                   'Permalink',
+                   'Post Engagement',
+                   'Reach',
+                   'Reporting Ends',
+                   'Reporting Starts',
+                   'Video Average Watch Time',
+                   'Video Percentage Watched',
+                   'Video Watches at 100%',
+                   'Video Watches at 25%',
+                   'Video Watches at 50%',
+                   'Video Watches at 75%',
+                   'Video Watches at 95%',
+                   'Website Purchases',
+                   'Website Searches',
+                   'Ad Name 8',
+                   'Tag',
+                   'in_out',
+                   'repeat_order']
 
+
+                  
 def fix_tag_headers(df):
     # creates a tag header naming convention
     new_columns = df.columns.values
@@ -23,12 +81,6 @@ def fix_tag_headers(df):
         i+=1
     return new_columns
 
-def divide_by_zero(num, dom):
-    if dom == 0:
-        return 0
-    else:
-        return num/dom
-
 
 def get_tag_list(df):
     # creates a list of tags
@@ -38,47 +90,56 @@ def get_tag_list(df):
             tag_list.append(column)
     return tag_list
 
+
 def get_tag_metrics(df, tag_list):
     rows_list = []
-    for item in tag_list:
-        row_dict = {}
-        total_amount_spent = 0
-        total_impressions = 0
-        total_website_purchases = 0
-        total_website_searches = 0
-        i = 0
-        for i in range(len(df[item])):
-            # totals for tag
-            if str(df[item][i]) == 'True':          
-                total_amount_spent += df['Amount Spent (USD)'][i]
-                total_impressions += df['Impressions'][i]
-                total_website_purchases += df['Website Purchases'][i]
-                total_website_searches += df['Website Searches'][i]
-        # metrics for analysis, add to new dataframe
-        row_dict['Tag'] = item
-        row_dict['total_amount_spent'] = total_amount_spent
-        row_dict['total_impressions'] = total_impressions
-        row_dict['total_website_purchases'] = total_website_purchases
-        row_dict['total_website_searches'] = total_website_searches
-        row_dict['price_per_purchase'] = divide_by_zero(total_amount_spent, total_website_purchases)
-        row_dict['price_per_search'] = divide_by_zero(total_amount_spent, total_website_searches)
-        row_dict['purchase_rate'] = divide_by_zero(total_website_purchases, total_impressions)
-        row_dict['search_rate'] = divide_by_zero(total_website_searches, total_impressions)
-        row_dict['purchase_per_search'] = divide_by_zero(total_website_searches, total_website_purchases)
-        rows_list.append(row_dict)
-    output_dataframe = pandas.DataFrame(rows_list)
-    return output_dataframe
+    for i in range(len(df)):
+        print('working on column {} of {}'.format(i, len(df)))
+        repeat_order = 1
+        for item in tag_list:
+            row_dict = {}
+
+            for header in useful_columns:
+                row_dict[header] = df[header][i]
+            if str(df[item][i]) == 'True':
+                row_dict['in_out'] = 'in'
+                row_dict['repeat_order'] = repeat_order
+                row_dict['Tag'] = item
+                repeat_order += 1
+            if str(df[item][i]) == 'False':
+                row_dict['in_out'] = 'out'
+                row_dict['repeat_order'] = repeat_order
+                row_dict['Tag'] = item
+                repeat_order += 1
+            rows_list.append(row_dict)
+    print('no memory issue here!')
+    return rows_list
+
+def write_rows(dictionary, outfile):
+    with open(outfile, 'w', newline='') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames = headers)
+        writer.writeheader()
+        for line in dictionary:
+            writer.writerow(line)
+
+
+
 
 
 
 def main(infile, outfile):
-    df = pandas.read_csv(infile)
+    df = pandas.read_csv(infile, engine='python')
+    print('it worked')
     df.columns = fix_tag_headers(df)
+    print('get tag list')
     tag_list = get_tag_list(df)
-    df[number_list] = df[number_list].fillna(0)
-    output_df = get_tag_metrics(df, tag_list)
-    output_df.to_csv(outfile)
+
+    dictionary_to_write = get_tag_metrics(df, tag_list)
+    write_rows(dictionary_to_write, outfile)
+    
+    complete_time = datetime.now() - startTime
+    print('Time to complete: '+ complete_time)
 
 
+main("TagDataJoinedPython.csv", "output2.csv")
 
-main("tag_data.csv", "output.csv")
